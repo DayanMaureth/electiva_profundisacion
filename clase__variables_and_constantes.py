bd7d5b2435f3596c4f7ef4a3240e9d3eec7ee235
DayanMaureth <dayantorres18.s@gmail.com>
2026-08-10 07:30:48 -0500

    Add area and perimeter calculations in Python

---
+++ clase__variables_and_constantes.py
@@ -0,0 +1,13 @@
+#Definir dimensiones 
+ancho = 5
+alto = 6
+
+#calcular area
+area = ancho * alto
+
+#calcular promedio
+perimetro = 2 * (ancho + alto)
+
+#mostrar resultados 
+print ("area:", area)
+print ("perimetro:", perimetro)
