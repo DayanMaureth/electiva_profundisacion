1e63bfb0682efe6ac1dca1fb3548a735e82b6744
DayanMaureth <dayantorres18.s@gmail.com>
2026-08-10 07:55:22 -0500

    Add age conversion to days in Python script

--- clase__variables_and_constantes.py
+++ clase__variables_and_constantes.py
@@ -11,3 +11,8 @@ perimetro = 2 * (ancho + alto)
 #mostrar resultados 
 print ("area:", area)
 print ("perimetro:", perimetro)
+
+#edad coversion: ERROR (str * int no funciona con años )
+edad = input ("¿cuantos años tienes?")
+dias = int (edad) * 365
+print("has vivido " + str(dias) + " dias" )
